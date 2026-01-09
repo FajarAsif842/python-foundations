2c8dee0f513fbde9d9aefc2b117bc7fa21e07330
FajarAsif842 <fajar.asif.dev@gmail.com>
2026-01-09 13:36:09 +0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,65 @@
+# #Print
+# print("Hello World")
+
+# name = "Fajar"
+# print(name)
+
+# #Data Types
+
+# a = 10
+# b= 2.5
+# c= "Python"
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# #Input
+# name = input("Enter your name :")
+# print("Hi👋",name)
+
+# #: Add Two Numbers
+# fnum = int(input("Enter First number: "))
+# lnum = int(input("Enter Secpnd second: "))
+# print("Sum:" ,fnum+lnum)
+
+# #Area of Rectangle
+# length = float(input("Enter lenght:"))
+# width = float(input("Enter width:"))
+# area = length *width
+# print("Area = ",area)
+
+# #Simple Calculator
+# a = int(input("Enter first number:"))
+# b = int(input("Enter Second number:"))
+
+# print("Add", a+b)
+# print("Subtract", a-b)
+# print("Muktiply", a*b)
+# print("Divide", a/b)
+
+# #SQUARE OF A NUMBER
+# number  = int(input("Enter a number"))
+# square = number*number
+# print("Square=",square)
+
+#Cube of a number
+# num = int(input("Enter a number"))
+# cube = num*num*num
+# print("Cube of a number:",cube)
+
+#Celsius to Fahrenheit
+# celsius = float(input("Enter temperature in Celsius:"))
+# fahrenheit = (celsius* 9/5)+32
+# print("Temperature in Fahreneit=",fahrenheit)
+
+#Age Calculator
+birth_year = int(input("Enter Birth year"))
+current_year = 2026
+age = current_year - birth_year
+print("Your age is:",age)
+
+
+
+
+
+

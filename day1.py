c4e6388dd86a5c48697845619eb70f7a567a0556
FajarAsif842 <fajar.asif.dev@gmail.com>
2026-01-09 13:53:17 +0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,56 +1,56 @@
-# #Print
-# print("Hello World")
+#Print
+print("Hello World")
 
-# name = "Fajar"
-# print(name)
+name = "Fajar"
+print(name)
 
-# #Data Types
+#Data Types
 
-# a = 10
-# b= 2.5
-# c= "Python"
-# print(type(a))
-# print(type(b))
-# print(type(c))
+a = 10
+b= 2.5
+c= "Python"
+print(type(a))
+print(type(b))
+print(type(c))
 
-# #Input
-# name = input("Enter your name :")
-# print("Hi👋",name)
+#Input
+name = input("Enter your name :")
+print("Hi👋",name)
 
-# #: Add Two Numbers
-# fnum = int(input("Enter First number: "))
-# lnum = int(input("Enter Secpnd second: "))
-# print("Sum:" ,fnum+lnum)
+#: Add Two Numbers
+fnum = int(input("Enter First number: "))
+lnum = int(input("Enter Secpnd second: "))
+print("Sum:" ,fnum+lnum)
 
-# #Area of Rectangle
-# length = float(input("Enter lenght:"))
-# width = float(input("Enter width:"))
-# area = length *width
-# print("Area = ",area)
+#Area of Rectangle
+length = float(input("Enter lenght:"))
+width = float(input("Enter width:"))
+area = length *width
+print("Area = ",area)
 
-# #Simple Calculator
-# a = int(input("Enter first number:"))
-# b = int(input("Enter Second number:"))
+#Simple Calculator
+a = int(input("Enter first number:"))
+b = int(input("Enter Second number:"))
 
-# print("Add", a+b)
-# print("Subtract", a-b)
-# print("Muktiply", a*b)
-# print("Divide", a/b)
+print("Add", a+b)
+print("Subtract", a-b)
+print("Muktiply", a*b)
+print("Divide", a/b)
 
-# #SQUARE OF A NUMBER
-# number  = int(input("Enter a number"))
-# square = number*number
-# print("Square=",square)
+#SQUARE OF A NUMBER
+number  = int(input("Enter a number"))
+square = number*number
+print("Square=",square)
 
 #Cube of a number
-# num = int(input("Enter a number"))
-# cube = num*num*num
-# print("Cube of a number:",cube)
+num = int(input("Enter a number"))
+cube = num*num*num
+print("Cube of a number:",cube)
 
 #Celsius to Fahrenheit
-# celsius = float(input("Enter temperature in Celsius:"))
-# fahrenheit = (celsius* 9/5)+32
-# print("Temperature in Fahreneit=",fahrenheit)
+celsius = float(input("Enter temperature in Celsius:"))
+fahrenheit = (celsius* 9/5)+32
+print("Temperature in Fahreneit=",fahrenheit)
 
 #Age Calculator
 birth_year = int(input("Enter Birth year"))
